c2ba2a83b068a342d80dd9649dfa2c6ecb6127ad
Jehan Veracierta <jehanzao@gmail.com>
2022-10-14 20:07:34 -0400

    modelo listo, sigue funcionando

--- src/controllers/jobs.controller.ts
+++ src/controllers/jobs.controller.ts
@@ -1,4 +1,4 @@
-import Jobs from '../models/jobs';
+import {Job} from '../models/jobs';
 
 export const getJobs = (req:any, res:any) => {
     res.send('holis')
--- src/models/jobs.ts
+++ src/models/jobs.ts
@@ -1,18 +1,35 @@
-import { ObjectId } from "mongodb";
-import { Url } from "url";
+import { Schema, model, connect } from 'mongoose';
 
-export default class Job {
-    constructor(
-        public id: ObjectId,
-        public title: string,
-        public companyName: string,
-        public description: string,
-        public experienceLevel: string,
-        public link: Url,
-        public ubication: string,
-        public remote: boolean,
-        public payRange: string,
-        public category: string,
-        public applicants: number,
-    ) {}
+// 1. Create an interface representing a document in MongoDB.
+interface IJob {
+  title: string;
+  companyName: string;
+  description: string;
+  ubication: string;
+  remote: boolean;
+  type: string;
+  applicants: number;
+  experienceLevel: string;
+  url: string;
+  payRange: {min: string, max: string};
+  category?: string;
 }
+
+// 2. Create a Schema corresponding to the document interface.
+const jobSchema = new Schema<IJob>({
+    title: { type: String, required: true },
+    companyName: { type: String, required: true },
+    description: { type: String, required: true },
+    ubication: { type: String, required: true },
+    remote: { type: Boolean, required: true },
+    type: { type: String, required: true },
+    applicants: { type: Number, required: true },
+    experienceLevel: { type: String, required: true },
+    url: { type: String, required: true },
+    payRange: {min: { type: String, required: true }, max: { type: String, required: true }},
+    category: { type: String, required: false }
+});
+
+// 3. Create a Model.
+export const Job = model<IJob>('Job', jobSchema);
+
